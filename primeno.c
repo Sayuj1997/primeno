26f919d3a782081b691388f4af55debb8ff6a6ff
Sayuj1997 <57167312+Sayuj1997@users.noreply.github.com>
2019-10-30 15:36:33 +0530

    Create primeno.c

---
+++ primeno.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //printf("Hello world!\n");
+    int n,flag=0;
+    printf("Enter the no.");
+    scanf("%d",&n);
+    for(int i=2;i<=n/2;i++)
+    {
+        if(n%i==0)
+        {
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0)
+    {
+     printf("It is a prime no");
+    }
+    else{
+        printf("it is not a prime no");
+    }
+  return 0;
+}
